0f5a9e04549dc870d3128cd655d1c677611a2327
Kalimaha <guido.barbaglia@gmail.com>
2015-02-24 13:33:01 +0100

    Bulk downloads added. Tree added.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ require.config({
 
     paths: {
 
-        //FAOSTAT_BULK_DOWNLOADS: root + 'faostat-bulk-downloads/faostat-bulk-downloads',
-        //faostat_bulk_downloads: root + 'faostat-bulk-downloads',
+        jquery : repository + 'jquery/1.10.2/jquery-1.10.2.min',
+
+        FAOSTAT_BULK_DOWNLOADS: root + 'faostat-bulk-downloads/faostat-bulk-downloads',
+        faostat_bulk_downloads: root + 'faostat-bulk-downloads',
         //
         //FAOSTAT_DOWNLOAD_OPTIONS: root + 'faostat-download-options/faostat-download-options',
         //faostat_download_options: root + 'faostat-download-options',
@@ -19,14 +21,38 @@ require.config({
         FAOSTAT_TREE: root + 'faostat-tree/faostat-tree',
         faostat_tree: root + 'faostat-tree'
 
+    },
+
+    shim: {
+
+        'faostat_bulk_downloads': {
+            deps :['jquery']
+        }
+
     }
 
 });
 
-require(['FAOSTAT_TREE'], function(TREE) {
-    console.log('hallo');
+require(['FAOSTAT_BULK_DOWNLOADS', 'FAOSTAT_TREE'], function(BULK, TREE) {
+
+    /* Language. */
+    var lang = 'S';
+
+    /* Initiate components. */
     var tree = new TREE();
+    var bulk = new BULK();
+
+    /* Initiate tree. */
     tree.init({
-        placeholder_id: 'left_placeholder'
+        placeholder_id: 'left_placeholder',
+        lang: lang
     });
+
+    /* Initiate bulk downloads. */
+    bulk.init({
+        placeholder_id: 'bulk_downloads_placeholder',
+        domain: 'GE',
+        lang: lang
+    });
+
 });
