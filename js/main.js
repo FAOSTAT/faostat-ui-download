c4f903dc9dd19fe16856edaa27ce0a2c7b18e352
Kalimaha <guido.barbaglia@gmail.com>
2015-02-27 16:27:19 +0100

    Summary controller.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var root = '../modules/';
 var repository = '//fenixapps.fao.org/repository/js/';
+var wds = 'http://faostat3.fao.org/wds/';
 
 require.config({
 
@@ -8,6 +9,7 @@ require.config({
     paths: {
 
         jquery : repository + 'jquery/1.10.2/jquery-1.10.2.min',
+        'bootstrap': repository + 'bootstrap/3.2/js/bootstrap.min',
 
         FAOSTAT_BULK_DOWNLOADS: root + 'faostat-bulk-downloads/faostat-bulk-downloads',
         faostat_bulk_downloads: root + 'faostat-bulk-downloads',
@@ -15,11 +17,8 @@ require.config({
         FAOSTAT_DOWNLOAD_OPTIONS: root + 'faostat-download-options/faostat-download-options',
         faostat_download_options: root + 'faostat-download-options',
 
-        FAOSTAT_DOWNLOAD_SUMMARY: root + 'faostat-download-summary-2/faostat-download-summary',
-        faostat_download_summary: root + 'faostat-download-summary-2',
-
-        FAOSTAT_DOWNLOAD_SELECTOR: root + 'faostat-download-summary-2/js/modules/faostat-download-selector/faostat-download-selector',
-        faostat_download_selector: root + 'faostat-download-summary-2/js/modules/faostat-download-selector',
+        FAOSTAT_DOWNLOAD_SELECTOR: root + 'faostat-selector/faostat-download-selector',
+        faostat_download_selector: root + 'faostat-selector',
 
         FAOSTAT_TREE: root + 'faostat-tree/faostat-tree',
         faostat_tree: root + 'faostat-tree',
@@ -30,6 +29,10 @@ require.config({
     },
 
     shim: {
+
+        'bootstrap': {
+            deps :['jquery']
+        },
 
         'faostat_bulk_downloads': {
             deps :['jquery']
@@ -42,19 +45,20 @@ require.config({
 require(['FAOSTAT_BULK_DOWNLOADS',
          'FAOSTAT_TREE',
          'FAOSTAT_DOWNLOAD_OPTIONS',
-         'FAOSTAT_DOWNLOAD_SUMMARY',
-         'FENIX_UI_METADATA_VIEWER'], function(BULK, TREE, OPTIONS, SUMMARY, METADATDA) {
+         'FENIX_UI_METADATA_VIEWER',
+         'FAOSTAT_DOWNLOAD_SELECTOR'], function(BULK, TREE, OPTIONS, METADATDA, SELECTOR) {
 
     /* Language. */
     var lang = 'S';
     var domain = 'GE';
+    var datasource = 'faostat';
 
     /* Initiate components. */
     var tree = new TREE();
     var bulk = new BULK();
     var options = new OPTIONS();
-    var summary = new SUMMARY();
     var metadata = new METADATDA();
+    var selector_1 = new SELECTOR();
 
     /* Initiate tree. */
     tree.init({
@@ -77,18 +81,25 @@ require(['FAOSTAT_BULK_DOWNLOADS',
         lang: lang
     });
 
-    /* Initiate summary. */
-    summary.init({
-        placeholder_id: 'options_placeholder',
-        lang: lang
-    });
-
     /* Initiate metadata. */
     metadata.init({
         placeholder_id: 'metadata_placeholder',
         domain: domain,
         lang: lang,
         view_type: 'accordion'
+    });
+
+    /* Initiate selector. */
+    var rest_1 = wds + 'rest/procedures/usp_GetListBox/' + datasource + '/' + domain + '/1/1/' + lang;
+    var rest_2 = wds + 'rest/procedures/usp_GetListBox/' + datasource + '/' + domain + '/1/2/' + lang;
+    selector_1.init({
+        lang: lang,
+        placeholder_id: 'selector_1_placeholder',
+        suffix: 'area',
+        tabs :   [
+            {label: 'Country', rest: rest_1},
+            {label: 'Group', rest: rest_2}
+        ]
     });
 
 });
