0f500fec3051bff6d6179ba020e5809559a36e83
Kalimaha <guido.barbaglia@gmail.com>
2015-11-23 17:27:28 +0100

    Work in progress: move code from view to module.

--- nls/fr/translate.js
+++ nls/fr/translate.js
@@ -1,3 +1,4 @@
+/*global define*/
 define({
     metadata: 'Métadonnées',
     bulk: 'Donnes Completes',
--- nls/translate.js
+++ nls/translate.js
@@ -1,11 +1,14 @@
+/*global define*/
 define({
     root : {
         error: 'FAOSTAT UI Download Error',
-        metadata: 'Metadata',
-        bulk: 'Bulk Downloads',
-        custom: 'Interactive Download',
-        preview: 'Preview',
-        faostat_domains_label: 'FAOSTAT Domains'
+        tree_title: 'FAOSTAT Domains',
+        preview_options_label: 'Preview Options',
+        download_as_label: 'Download as...',
+        preview_label: 'Preview',
+        metadata_label: 'Metadata',
+        bulk_downloads_label: 'Bulk Downloads',
+        interactive_download_label: 'Interactive Download'
     },
     fr : true,
     es : true
