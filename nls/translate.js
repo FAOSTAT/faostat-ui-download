276825ddb55d6e02317e63593fb8255ce892f8f2
simonemurzilli <simone.murzilli@fao.org>
2016-01-29 15:44:26 +0100

    refactoring

---
+++ nls/translate.js
@@ -0,0 +1,11 @@
+/*global define*/
+define({
+    root : {
+
+        download_as: 'Download',
+        preview: 'Preview',
+        interactive_download_welcome: 'Use this section to create a custom query to extract data from the current domain. Please use the selectors below to filter the data according to your needs. Press the <var>Preview</var> button to have an overview of your selection. Use the <var>Preview Options</var> button to customize the final output. Click on <var>Download As...</var> to get the data in the Excel or CSV format. Please consider to visit the <var>Bulk Downloads</var> sections if you need all the data contained in this domain.',
+    },
+    fr : true,
+    es : true
+});
